aae75b1db8397d9c224cb22d0e080eb9bab3d0dc
ChillerDragon <ChillerDragon@gmail.com>
2022-08-23 09:30:25 +0200

    Add username field to view and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    # from devise docs
+    # https://github.com/heartcombo/devise#strong-parameters
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
 end
