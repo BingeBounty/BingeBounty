064ca4f3241481c2a0af433ea9b1255eb1a13704
ChillerDragon <ChillerDragon@gmail.com>
2022-08-23 12:42:12 +0200

    Rails generate model Article
    rails generate model Article \
    	title:string \
    	body:text \
    	user:references \
    	source_url:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_070600) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_23_104140) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.string "source_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_070600) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "articles", "users"
 end
