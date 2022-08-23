dc6c3f26c773633a74c4787466e95c2c715001a9
ChillerDragon <ChillerDragon@gmail.com>
2022-08-23 10:13:59 +0200

    Fix username field on user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
+  protected
 
-    # from devise docs
-    # https://github.com/heartcombo/devise#strong-parameters
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    end
+  # from devise docs
+  # https://github.com/heartcombo/devise#strong-parameters
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up)  {|u| u.permit(:username, :email, :password, :password_confirmation, :remember_me)}
+    # devise_parameter_sanitizer.permit(:sign_in)  {|u| u.permit(:username, :email, :password, :password_confirmation, :remember_me)}
+    # devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:email, :username, :password, :password_confirmation, :remember_me)}
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_070600) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
+    t.string "username", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
