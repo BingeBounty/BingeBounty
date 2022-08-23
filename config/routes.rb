d93e776bdc02fae0729e08c10980eaa3ba4480d2
ChillerDragon <ChillerDragon@gmail.com>
2022-08-23 16:27:58 +0200

    Use ressources for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'articles/index', to: 'articles#index'
-  get 'articles/show', to: 'articles#show'
-  get 'articles/new', to: 'articles#new'
-  post 'articles/new', to: 'articles#create'
-  post 'articles/', to: 'articles#create', as: :articles
+  # get 'articles/index', to: 'articles#index'
+  # get 'articles/show', to: 'articles#show'
+  # get 'articles/new', to: 'articles#new'
+  # post 'articles/new', to: 'articles#create'
+  # post 'articles/', to: 'articles#create', as: :articles
 
-  # resources :articles, only: [:new, :create]
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  resources :articles
   root "articles#index"
 end
